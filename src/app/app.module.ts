dfc6ec60d4a382f1d02762b46928853b06c35d2f
jmuzsik <jerrymuzsik@icloud.com>
2018-05-22 01:53:00 -0400

    a few minor edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,8 @@ import { BookComponent } from "./book/book.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { BookService } from "./book.service";
 import { LoadingComponent } from "./loading/loading.component";
-
 import * as bootstrap from "bootstrap";
+
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
 
